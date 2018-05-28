740edd60b4bcd9cee30f2456edd9926f9475f6ff
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2018-05-28 11:37:58 +0200

    Solution to rest operator as parameters

---
+++ test/session-2-rest-operator-as-parameter.js
@@ -0,0 +1,36 @@
+// IMPORTANTE: Es necesario añadir esto para que funcione correctamente el ejercicio aqui, si quieres probarlo en la web es mejor que lo comentes
+import './config.js'
+
+// 18: rest - as-parameter
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('rest in function params', () => {
+
+  it('must be the last parameter', () => {
+    const fn = (...rest) => {
+      assert.deepEqual([1, 2], rest);
+    };
+    fn(1, 2);
+  });
+
+  it('can be used to get all other parameters', () => {
+    const fn = (firstParam, secondParam, ...rest) => {
+      assert.deepEqual([3,4], rest);
+    };
+    fn(null, 2, 3, 4);
+  });
+
+  it('makes `arguments` obsolete', () => {
+    const fn = (...args) => {
+      assert.deepEqual([42, 'twenty three', 'win'], args);
+    };
+    fn(42, 'twenty three', 'win');
+  });
+
+  it('eliminate `arguments`!!!', () => {
+    const fn = (...args) => args;
+    const [firstArg, ...rest] = fn(1, 2, 3);
+    assert.deepEqual([2, 3], rest);
+  });
+
+});
